d04c490cbf46f2b2ed153508b098c1b190b9993d
Avishek Gupta <79639472+Avishek98@users.noreply.github.com>
2022-04-13 23:47:26 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+ <div class="wapper col5">
+
+  <div id="footer">
+
+    <div id="copyright">
+      <p class="fl_left">Copyright &copy; 2021 - All Rights Reserved | <a href="adminlogin.php">Admin Login Panel</a> | <a href="doctorlogin.php">Doctor Login Panel</a></p>
+      <p class="fl_right">Developed by Meghna Roy Arka Chakraborty Aniruddha Das Atin Das</p>
+      <br class="clear" />
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+</body>
+</html>
